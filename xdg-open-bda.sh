2c3dcc5106e63acd1e66f453f98bc21aaa744d6a
harriott <trohib@gmail.com>
2015-01-28 16:42:04 +0100

    got xdg-open-dba working for Arch

--- xdg-open-bda.sh
+++ xdg-open-bda.sh
@@ -4,7 +4,8 @@
 
 # Blue-Dog-Archolite's fix for gx https://github.com/tpope/vim-fugitive/issues/536
 # chmod 755 $HOME/.vim/xdg-open-bda.sh
-# ln -s $HOME/.vim/xdg-open-bda.sh $HOME/bin/xdg-open-bda
+# LM17Xfce64: ln -s $HOME/.vim/xdg-open-bda.sh $HOME/bin/xdg-open-bda
+# Arch Xfce: sudo ln -s $HOME/.vim/xdg-open-bda.sh /usr/local/bin/xdg-open-bda
 # and that symbolic link is referenced by netrw from my $HOME/.vim/debian.vim
 
 xdg-open "$@" &
