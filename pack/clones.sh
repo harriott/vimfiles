7081c68bcbee426e9db0f66a9364ef451d67aaef
Joseph Harriott <trohib@gmail.com>
2019-09-19 23:22:48 +0200

    vim-flavored-markdown

--- pack/clones.sh
+++ pack/clones.sh
@@ -9,13 +9,18 @@ if [ -d $packFolder ]; then cd $packFolder
   cd ../..
 else echo "no $packFolder"; fi
 
+packFolder="ft/start"
+if [ -d $packFolder ]; then cd $packFolder
+  git clone https://github.com/jtratner/vim-flavored-markdown
+  cd ../..
+else echo -en "\e[1m no $packFolder \e[0m"; echo; fi
+
 packFolder="layout/start"
 if [ -d $packFolder ]; then cd $packFolder
   git clone https://github.com/vim-scripts/gitignore.vim
   cd ../..
-else echo -en "\e[1m no $packFolder \e[0m"; fi
+else echo -en "\e[1m no $packFolder \e[0m"; echo; fi
 
-# gitignore.vim
 # vim-flavored-markdown
 # vim-ps1
 # vim-ingo-library
